52a1b2222ae644795faf4842454f812572eeac66
Henry <hennadii.lytvyn@gmail.com>
2025-08-14 14:57:34 +0100

    small bags edit

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -6,6 +6,8 @@ const body = document.querySelector('body');
 const mobileMenuBG = document.querySelector(
   '[data-id="mobile-menu__background"]'
 );
+const logo = document.querySelector('[data-id="header__logo"]');
+
 let scrollY;
 let heroImgHight;
 
@@ -23,14 +25,15 @@ mobileBtn.addEventListener('click', () => {
   heroImgCheck(Number(scrollY));
 });
 
-navLinks.forEach(link =>
-  link.addEventListener('click', () => {
-    navIcon.classList.remove('open');
-    mobileMenu.classList.remove('open');
-    mobileMenuBG.classList.remove('open');
-    body.classList.remove('no-scroll');
-  })
-);
+navLinks.forEach(link => link.addEventListener('click', closeMobileMenu));
+logo.addEventListener('click', closeMobileMenu);
+
+function closeMobileMenu() {
+  navIcon.classList.remove('open');
+  mobileMenu.classList.remove('open');
+  mobileMenuBG.classList.remove('open');
+  body.classList.remove('no-scroll');
+}
 
 function heroImgCheck(scrollY) {
   mobileMenu.style.backgroundPosition = `center -${scrollY}px`;
