62f5f67a1b042c52728e87c428f78a6dfcc7b08b
Henry <hennadii.lytvyn@gmail.com>
2025-08-22 20:03:47 +0100

    reviews card grade add

---
+++ src/js/reviews/reviews-card-markup.js
@@ -0,0 +1,30 @@
+const reviewsArr = [
+  {
+    name: 'Alexander Winter',
+    review:
+      'An epic saga of strategy and emotion. I’ve never cared this much about my army!',
+    grade: 5,
+  },
+  {
+    name: 'Mira Howard',
+    review: 'The decisions actually matter! Every campaign feels different',
+    grade: 5,
+  },
+  {
+    name: 'Dante Kurphy',
+    review:
+      'The mix of tactical gameplay and deep lore is masterful. Highly addictive',
+    grade: 5,
+  },
+  {
+    name: 'Selina Russel',
+    review:
+      'Heroes have actual personality — I grew attached to every general in my roster',
+    grade: 4,
+  },
+  {
+    name: 'Zhao Ling',
+    review: 'Best war strategy RPG on mobile. The visuals are on another level',
+    grade: 5,
+  },
+];
